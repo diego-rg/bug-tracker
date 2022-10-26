ad80f361835645e52df95c1428a94b2bfa3127bd
eu <diego_thegarden@hotmail.com>
2022-10-26 21:31:08 +0200

    post a bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import BugList from "./components/BugList";
+import CreateBug from "./components/CreateBug";
 
 function App() {
   return (
     <div>
       <h1>Bug tracker</h1>
       <BugList />
+      <CreateBug />
     </div>
   );
 }
---
+++ src/components/CreateBug.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import bugsAPI from "../apis/bugs";
+import BugForm from "./BugForm";
+
+const CreateBug = () => {
+  const [formValues, setformValues] = useState({
+    name: "",
+    description: "",
+    status: "",
+    priority: "",
+    severity: "",
+  });
+
+  const onSubmit = async (bugFormData) => {
+    try {
+      await bugsAPI.post("/bugs", bugFormData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <BugForm initialValues={formValues} onSubmit={onSubmit}>
+      Submit Bug
+    </BugForm>
+  );
+};
+
+export default CreateBug;
