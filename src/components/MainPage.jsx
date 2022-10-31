8a62ae3c2500ea52f4515450b0dfade07fc613d6
eu <diego_thegarden@hotmail.com>
2022-10-31 23:34:35 +0100

    open mobile menu

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,17 @@
+import React, { useState } from "react";
+
 import DesktopSidebar from "./DesktopSidebar";
 import MobileNabvar from "./MobileNavbar";
 import MobileSidebar from "./MobileSidebar";
 
 const MainPage = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
       <DesktopSidebar />
-      <MobileSidebar />
-      <MobileNabvar />
+      <MobileSidebar openMenu={openMenu} />
+      <MobileNabvar openMenu={openMenu} setOpenMenu={setOpenMenu} />
     </div>
   );
 };
--- src/components/MobileSidebar.jsx
+++ src/components/MobileSidebar.jsx
@@ -6,75 +6,77 @@ import {
 } from "react-icons/ri";
 import { MdOutlineCreateNewFolder } from "react-icons/md";
 
-const MobileSidebar = () => {
-  return (
-    <nav className="fixed inset-y-0 z-20 flex-shrink-0 w-44 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden">
-      <div className="py-4 text-gray-500 dark:text-gray-400">
-        <a
-          className="flex justify-center text-lg font-bold text-gray-800 dark:text-gray-200"
-          href="localhost:3000"
-        >
-          <img
-            className="w-8 inline"
-            src="/favicon-white.ico"
-            alt="Bug logo."
-          />
-          <span className="mr-3">Bug Tracker</span>
-        </a>
+const MobileSidebar = (props) => {
+  if (props.openMenu && window.innerWidth < 768) {
+    return (
+      <nav className="fixed inset-y-0 z-20 flex-shrink-0 w-44 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden">
+        <div className="py-4 text-gray-500 dark:text-gray-400">
+          <a
+            className="flex justify-center text-lg font-bold text-gray-800 dark:text-gray-200"
+            href="localhost:3000"
+          >
+            <img
+              className="w-8 inline"
+              src="/favicon-white.ico"
+              alt="Bug logo."
+            />
+            <span className="mr-3">Bug Tracker</span>
+          </a>
 
-        <ul className="mt-6">
-          <li className="relative px-6 py-4">
-            <a
-              className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-              href="../index.html"
-            >
-              <MdOutlineCreateNewFolder size={40} />
-              <span>Create Project</span>
-            </a>
-          </li>
-          <li className="relative px-6 py-4">
-            <a
-              className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-              href="../index.html"
-            >
-              <RiAddCircleLine size={40} />
-              <span>Create Bug</span>
-            </a>
-          </li>
+          <ul className="mt-6">
+            <li className="relative px-6 py-4">
+              <a
+                className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                href="../index.html"
+              >
+                <MdOutlineCreateNewFolder size={40} />
+                <span>Create Project</span>
+              </a>
+            </li>
+            <li className="relative px-6 py-4">
+              <a
+                className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                href="../index.html"
+              >
+                <RiAddCircleLine size={40} />
+                <span>Create Bug</span>
+              </a>
+            </li>
 
-          <li className="relative px-6 py-4">
-            <a
-              className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-              href="../index.html"
-            >
-              <RiBookOpenLine size={40} />
-              <span>Documentation</span>
-            </a>
-          </li>
+            <li className="relative px-6 py-4">
+              <a
+                className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                href="../index.html"
+              >
+                <RiBookOpenLine size={40} />
+                <span>Documentation</span>
+              </a>
+            </li>
 
-          <li className="relative px-6 py-4">
-            <a
-              className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-              href="../index.html"
-            >
-              <RiQuestionLine size={40} />
-              <span>About</span>
-            </a>
-          </li>
+            <li className="relative px-6 py-4">
+              <a
+                className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                href="../index.html"
+              >
+                <RiQuestionLine size={40} />
+                <span>About</span>
+              </a>
+            </li>
 
-          <li className="relative px-6 py-4">
-            <a
-              className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-              href="../index.html"
-            >
-              <RiLogoutBoxLine size={40} />
-              <span>Log out</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+            <li className="relative px-6 py-4">
+              <a
+                className="inline-flex flex-col items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                href="../index.html"
+              >
+                <RiLogoutBoxLine size={40} />
+                <span>Log out</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 };
 
 export default MobileSidebar;
