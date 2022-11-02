890d2c7ace34a752bde6ec86b39fad93407a9a45
eu <diego_thegarden@hotmail.com>
2022-11-02 18:29:59 +0100

    bug details card

--- src/components/BugDetails.jsx
+++ src/components/BugDetails.jsx
@@ -5,9 +5,7 @@ const BugDetails = (props) => {
     <div className="modal-container">
       <div className="modal-content">
         <div className="modal-header">
-          <h3 className="text-xl font-semibold text-gray-900 dark:text-white overflow-x-hidden">
-            {props.bugData.name}
-          </h3>
+          <h3 className="modal-title">{props.bugData.name}</h3>
           <button
             onClick={() => props.setShow(false)}
             type="button"
@@ -18,20 +16,17 @@ const BugDetails = (props) => {
           </button>
         </div>
 
-        <ul className="flex flex-col text-sm text-gray-500 dark:text-gray-300">
-          <li>Status: {props.bugData.status}</li>
-          <li>Priority: {props.bugData.priority}</li>
-          <li>Severity: {props.bugData.severity}</li>
-        </ul>
+        <div className="p-4">
+          <p className="modal-description">{props.bugData.description}</p>
 
-        <p className="mb-3 text-gray-700 dark:text-gray-400">
-          {props.bugData.description}
-        </p>
-
-        <ul className="flex flex-col text-sm text-gray-500 dark:text-gray-300">
-          <li>Created: {props.bugData.createdAt}</li>
-          <li>Last update: {props.bugData.updatedAt}</li>
-        </ul>
+          <ul className="modal-list">
+            <li>Status: {props.bugData.status}</li>
+            <li>Priority: {props.bugData.priority}</li>
+            <li>Severity: {props.bugData.severity}</li>
+            <li>Creation date: {props.bugData.createdAt}</li>
+            <li>Last update: {props.bugData.updatedAt}</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
